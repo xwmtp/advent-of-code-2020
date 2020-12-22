45a4ea6ffd91da9feaa57c27b7d4c4ba99eb5b11
xwmtp <xwillmtp@gmail.com>
2020-12-22 18:21:11 +0100

    Day 22 (decreased runtime from 20s to 5s)

--- Day22/Day22.py
+++ Day22/Day22.py
@@ -3,6 +3,9 @@
 with open('Input22.txt', 'r') as file:
     inputs = file.read().split('\n\n')
 
+def calculate_score(deck):
+    return sum((i + 1) * card for i, card in enumerate(deck))
+
 class Combat:
 
     def __init__(self, deck_p1, deck_p2):
@@ -42,9 +45,8 @@ deck_p1 = [int(l) for l in inputs[0].splitlines()[1:]][::-1]
 deck_p2 = [int(l) for l in inputs[1].splitlines()[1:]][::-1]
 
 game = Combat(deck_p1, deck_p2)
-_, winner_cards = game.play_game()
-score = sum((i + 1) * card for i, card in enumerate(winner_cards))
-print(score)
+_, winner_deck = game.play_game()
+print(calculate_score(winner_deck))
 
 
 # --- Part 2 --- #
@@ -54,19 +56,23 @@ class RecursiveCombat(Combat):
 
     def __init__(self, deck_p1, deck_p2):
         super().__init__(deck_p1, deck_p2)
-        self.past_configurations = []
+        self.past_configurations = {}
 
     def play_game(self):
         RecursiveCombat.games_started += 1
-        return super(RecursiveCombat, self).play_game()
+        result = super(RecursiveCombat, self).play_game()
+        return result
 
     def play_round(self):
-        self.past_configurations.append((self.deck_p1.copy(), self.deck_p2.copy()))
+        sum_p1 = sum(self.deck_p1)
+        if sum_p1 not in self.past_configurations:
+            self.past_configurations[sum_p1] = []
+        self.past_configurations[sum_p1].append((self.deck_p1.copy(), self.deck_p2.copy()))
         card_p1 = self.deck_p1.pop()
         card_p2 = self.deck_p2.pop()
         if (self.play_recurse_game(card_p1, card_p2)):
             recurse_game = RecursiveCombat(self.deck_p1[-card_p1:].copy(), self.deck_p2[-card_p2:].copy())
-            winner, winner_cards = recurse_game.play_game()
+            winner, winner_deck = recurse_game.play_game()
         else:
             winner = 'p1' if card_p1 > card_p2 else 'p2'
         if winner == 'p1':
@@ -79,7 +85,10 @@ class RecursiveCombat(Combat):
         return len(self.deck_p1) >= card_p1 and len(self.deck_p2) >= card_p2
 
     def repeated_configuration(self):
-        return (self.deck_p1, self.deck_p2) in self.past_configurations
+        sum_p1 = sum(self.deck_p1)
+        if sum_p1 in self.past_configurations:
+            return (self.deck_p1, self.deck_p2) in self.past_configurations[sum_p1]
+        return False
 
     def game_over(self):
         return super().game_over() or self.repeated_configuration()
@@ -88,6 +97,5 @@ class RecursiveCombat(Combat):
         return f'Game: {RecursiveCombat.games_started}\n{super().__str__()}'
 
 game = RecursiveCombat(deck_p1, deck_p2)
-_, winner_cards = game.play_game()
-score = sum((i + 1) * card for i, card in enumerate(winner_cards))
-print(score)
+_, winner_deck = game.play_game()
+print(calculate_score(winner_deck))
